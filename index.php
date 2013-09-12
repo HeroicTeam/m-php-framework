8718041ac7bce937476dcb774ca747c8fcf05cdd
Nicholas Glenn <work@nick-glenn.com>
2013-09-12 01:07:00 -0700

    Added public directory, moved index

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
 
-// Load our set up files
-include __DIR__.'/vendor/autoload.php';
-include __DIR__.'/app/containers.php';
-include __DIR__.'/app/routes.php';
-
+include __DIR__.'/public/index.php';
